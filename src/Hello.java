2ca05215a2ceb87fc59b02507457d4d4f5a866d0
jair-wang <sfswjr@126.com>
2020-08-25 13:40:17 +0800

    test

---
+++ src/Hello.java
@@ -0,0 +1,16 @@
+public class Hello{
+//  public static void main(String[] args){
+//    System.out.println("hello world");
+//    System.out.println("hihi");
+//    for(int i = 0 ;i<1;i++)
+//    {
+//    	
+//    }
+//    
+//  }
+
+@Override
+public String toString() {
+	return "Hello [toString()=" + super.toString() + "]";
+}
+}
